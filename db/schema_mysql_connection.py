0d628e8e0ed298475ee620b6b39bd2324934ab46
Luca Battesini <lucabattesini1@gmail.com>
2025-06-09 10:16:24 -0400

    Creating a schema to the connection with the db

---
+++ db/schema_mysql_connection.py
@@ -0,0 +1,12 @@
+#import mysql.connector
+#
+## Connect to the db
+#connection = mysql.connector.connect(
+#    host="localhost",
+#    user="your_user_name",
+#    password="your_password",
+#    database="database_name"
+#)
+#
+## Create a cursor
+#cursor = connection.cursor()
