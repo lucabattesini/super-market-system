df4a0e257268e73f3cbdfcb34e98d102006d0982
Luca Battesini <lucabattesini1@gmail.com>
2025-06-09 10:49:45 -0400

    Creating the structure to the main table

--- db/migrations.py
+++ db/migrations.py
@@ -1,4 +1,4 @@
 from connection import cursor
 
 # Create a table
-cursor.execute("CREATE TABLE ()")
+cursor.execute("CREATE TABLE product_info (id, name, price, description, category, brand, weight, unit, is_active)")
