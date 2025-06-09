f4964f936323bef8503fb362ae1505a4f4073d50
Luca Battesini <lucabattesini1@gmail.com>
2025-06-09 10:59:21 -0400

    Changing the formate the command create the table, now the table is created

--- db/migrations.py
+++ db/migrations.py
@@ -1,4 +1,4 @@
 from connection import cursor
 
 # Create a table
-cursor.execute("CREATE TABLE product_info (id, name, price, description, category, brand, weight, unit, is_active)")
+cursor.execute("CREATE TABLE product_info (id VARCHAR(36) PRIMARY KEY, name VARCHAR(100), price DECIMAL(10, 2), description TEXT, category VARCHAR(50), brand VARCHAR(50), weight DECIMAL(10, 2), unit VARCHAR(10), is_active BOOLEAN);")
