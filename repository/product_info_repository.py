61362bc382e83c15c2b044113914e8866d55bf29
Luca Battesini <lucabattesini1@gmail.com>
2025-06-11 09:56:04 -0400

    CCorrecting a syntax error in edit_product function

--- repository/product_info_repository.py
+++ repository/product_info_repository.py
@@ -49,5 +49,5 @@ def edit_product(id, name, price, description, category, brand, weight, unit, is
     '''
     Edit a product
     '''
-    cursor.execute(f"UPDATE product_info SET name = '{name}', price = '{price}', description = '{description}', category = '{category}', brand = '{brand}', weight = '{weight}', unit = '{unit}', is_active = '{is_active}' WHERE id = '{id}';")
+    cursor.execute(f"UPDATE product_info SET name = '{name}', price = '{price}', description = '{description}', category = '{category}', brand = '{brand}', weight = '{weight}', unit = '{unit}', is_active = {is_active} WHERE id = '{id}';")
     connection.commit()
