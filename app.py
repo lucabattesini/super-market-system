41f1273010236fd3167aa12cdd4d1301c6f4ffe2
Luca Battesini <lucabattesini1@gmail.com>
2025-06-11 11:22:11 -0400

    Adding inventory route to the app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from routers import product_info
+from routers import product_info, product_stock
 
 app = FastAPI()
 
 app.include_router(product_info.router)
+app.include_router(product_stock.router)
