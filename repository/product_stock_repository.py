b132598319399b3e36b7f7290f52e178286d7f23
Luca Battesini <lucabattesini1@gmail.com>
2025-06-12 10:55:13 -0400

    Creating the structure to all the products

--- repository/product_stock_repository.py
+++ repository/product_stock_repository.py
@@ -1,2 +1,14 @@
 from db.connection import cursor, connection
 from schemas.products_stock_schema import ProductStock
+
+def get_all_products_from_stock():
+    return
+
+def disable_product_from_stock():
+    return
+
+def create_product_in_stock():
+    return
+
+def edit_product_stock_quantity():
+    return
