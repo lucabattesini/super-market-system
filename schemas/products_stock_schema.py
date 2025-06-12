f5b232ee8721bcd3b5fbb246294209decc3f3345
Luca Battesini <lucabattesini1@gmail.com>
2025-06-12 11:22:14 -0400

    Changing the structure of product_stock

--- schemas/products_stock_schema.py
+++ schemas/products_stock_schema.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
 class ProductStock(BaseModel):
-    bar_code: str
     product_id: str
+    bar_code: str
     overall_stock: int
     store_stock: int
     warehouse_stock: int
+    last_updated: str
+    is_active: bool
