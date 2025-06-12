3fc8f37694196f29b194706640872a2c2ffbd8d7
Luca Battesini <lucabattesini1@gmail.com>
2025-06-12 11:07:39 -0400

    Creating the function wich will organize the in stock products

--- repository/product_stock_repository.py
+++ repository/product_stock_repository.py
@@ -1,6 +1,18 @@
 from db.connection import cursor, connection
 from schemas.products_stock_schema import ProductStock
 
+def parse_product_from_stock(params) -> ProductStock:
+    '''
+    Organize the vars into an object format
+    '''
+    return ProductStock(
+        product_id=params[0],
+        bar_code=params[1],
+        overall_stock=params[2],
+        store_stock=params[3],
+        warehouse_stock=params[4]
+        )
+
 def get_all_products_from_stock():
     return
 
