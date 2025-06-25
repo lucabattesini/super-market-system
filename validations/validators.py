82842b80676b0c9d812d1a993982aab2bbaf9f14
Luca Battesini <lucabattesini1@gmail.com>
2025-06-25 14:36:03 -0400

    Creating a function wich will create a product in both tables at the same time with the same id

--- validations/validators.py
+++ validations/validators.py
@@ -2,9 +2,6 @@ from db.connection import cursor, connection
 from repository.product_info_repository import create_product
 from repository.product_stock_repository import create_product_in_stock
 
-def create_product_in_both_tables():
-    return
-
 def check_if_id_already_exist(id, table):
     '''
     Check if an id already exists in an specific table
@@ -19,3 +16,20 @@ def check_if_id_already_exist(id, table):
         return False
     elif id_exists > 0:
         return True
+    
+
+def create_product_in_both_tables(id, bar_code, name, price, description, category, brand, weight, unit, is_active):
+    '''
+    Will create a product in both tables wich depends from each other
+    '''
+    check_info_table = check_if_id_already_exist(id, "product_info")
+    check_stock_table = check_if_id_already_exist(id, "product_stock")
+    if check_info_table == False:
+        if check_stock_table == False:
+            create_product(id, name, price, description, category, brand, weight, unit, is_active)
+            create_product_in_stock(id, bar_code)
+            return {"message": "Product successfully created"}
+        else:
+            return {"message": "ID already exist"}
+    else: 
+        return {"message": "ID already exist"}
