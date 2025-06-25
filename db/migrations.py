e87e9ad57d5dceecf6b89a3225b36038ee1b070a
Luca Battesini <lucabattesini1@gmail.com>
2025-06-25 14:08:36 -0400

    Creating a function wich check if an id already exist

--- db/migrations.py
+++ db/migrations.py
@@ -3,4 +3,4 @@ from connection import cursor
 # Create a table
 cursor.execute("CREATE TABLE product_info (id VARCHAR(36) PRIMARY KEY, name VARCHAR(100), price DECIMAL(10, 2), description TEXT, category VARCHAR(50), brand VARCHAR(50), weight DECIMAL(10, 2), unit VARCHAR(10), is_active BOOLEAN);")
 
-cursor.execute("CREATE TABLE product_stock (product_id VARCHAR(50) NOT NULL, bar_code VARCHAR(50) NOT NULL UNIQUE, overall_stock INT NOT NULL, store_stock INT NOT NULL, warehouse_stock INT NOT NULL, last_updated DATETIME NOT NULL, PRIMARY KEY (product_id));")
+cursor.execute("CREATE TABLE product_stock (id VARCHAR(50) NOT NULL, bar_code VARCHAR(50) NOT NULL UNIQUE, overall_stock INT NOT NULL, store_stock INT NOT NULL, warehouse_stock INT NOT NULL, last_updated DATETIME NOT NULL, PRIMARY KEY (id));")
--- validations/validators.py
+++ validations/validators.py
@@ -1,1 +1,21 @@
+from db.connection import cursor, connection
+from repository.product_info_repository import create_product
+from repository.product_stock_repository import create_product_in_stock
 
+def create_product_in_both_tables():
+    return
+
+def check_if_id_already_exist(id, table):
+    '''
+    Check if an id already exists in an specific table
+    '''
+    cursor.execute("SELECT * FROM %s", (table))
+    ids_list = cursor.fetchall()
+    id_exists = int(0)
+    for r in ids_list:
+        if r == id:
+            id_exists = id_exists + 1
+    if id_exists == 0:
+        return False
+    elif id_exists > 0:
+        return True
