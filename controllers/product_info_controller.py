9bed4873481c8fd567c1286ac33b4fb9b5a655c3
Luca Battesini <lucabattesini1@gmail.com>
2025-07-02 11:43:15 -0400

    Adding get_product_by_id to controllers

--- controllers/product_info_controller.py
+++ controllers/product_info_controller.py
@@ -12,6 +12,14 @@ async def get_all_products():
     json_result = jsonable_encoder(products)
     return json_result
 
+async def get_product_by_id(id):
+    '''
+    Function used to return and product selected by id
+    '''
+    product = product_info_repo.get_product_by_id(id)
+    json_result = jsonable_encoder(product)
+    return json_result
+
 def create_product_in_both_tables(bar_code, name, price, description, category, brand, weight, unit, is_active):
     '''
     Will create a product in both tables which depends from each other
