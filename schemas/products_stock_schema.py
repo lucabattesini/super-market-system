db7be48f5b6da1db488e19e8e818b9c0b05e278b
Luca Battesini <lucabattesini1@gmail.com>
2025-07-03 10:45:40 -0400

    Creating a new schema to the operation in stock quantity

--- schemas/products_stock_schema.py
+++ schemas/products_stock_schema.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from pydantic import BaseModel
 
 class ProductStock(BaseModel):
@@ -7,3 +8,8 @@ class ProductStock(BaseModel):
     store_stock: int
     warehouse_stock: int
     last_updated: str
+
+class ProductStockOperation(BaseModel):
+    id: str
+    data: int
+    operation: Literal["add", "sub"]
