247e6580093bc2849328bc987de9d24ef34a02a4
Luca Battesini <lucabattesini1@gmail.com>
2025-07-07 11:03:18 -0400

    Creating a function in controllers which will edit stock quantity

--- controllers/product_stock_controller.py
+++ controllers/product_stock_controller.py
@@ -1,4 +1,10 @@
 from repository.product_stock_repo import edit_product_quantity_in_stock
+from repository.product_stock_repo import edit_product_quantity_in_stock, get_product_id_by_bar_code
 
-def edit_different_products_quantity_in_stock(bar_codes):
-    return
+def edit_different_products_quantity_in_stock(products):
+    for r in products:
+        bar_code = r["bar_code"]
+        quantity = r["quantity"]
+        id = get_product_id_by_bar_code(bar_code)
+        edit_product_quantity_in_stock(id, quantity, "sub")
+    return {"message": "products edited successfully"}
