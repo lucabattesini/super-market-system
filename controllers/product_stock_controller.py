4ec668f648da58f49c624465f4f88bd320d3843a
Luca Battesini <lucabattesini1@gmail.com>
2025-07-08 10:50:09 -0400

    Creating a function in controllers which will turn all the data from get_all_products_from_stock to json format

--- controllers/product_stock_controller.py
+++ controllers/product_stock_controller.py
@@ -1,5 +1,5 @@
-from repository.product_stock_repo import edit_product_quantity_in_stock
-from repository.product_stock_repo import edit_product_quantity_in_stock, get_product_id_by_bar_code
+from fastapi.encoders import jsonable_encoder
+from repository.product_stock_repo import edit_product_quantity_in_stock, get_product_id_by_bar_code, edit_product_quantity_in_stock, get_all_products_from_stock
 
 def edit_different_products_quantity_in_stock(products):
     for r in products:
@@ -8,3 +8,8 @@ def edit_different_products_quantity_in_stock(products):
         id = str(get_product_id_by_bar_code(bar_code))
         edit_product_quantity_in_stock(id, quantity, "sub")
     return {"message": "products edited successfully"}
+
+def get_all_products_from_stock_json():
+    products = get_all_products_from_stock()
+    json_result = jsonable_encoder(products)
+    return json_result
