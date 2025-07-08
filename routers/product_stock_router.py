fb5e2e89edd0811a02f8a79b48833d7dd564f6a3
Luca Battesini <lucabattesini1@gmail.com>
2025-07-08 10:49:32 -0400

    Adding personalized status codde to all the routes

--- routers/product_stock_router.py
+++ routers/product_stock_router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, status
 from typing import List
 from fastapi.responses import JSONResponse
-from repository.product_stock_repo import get_all_products_from_stock, edit_product_quantity_in_stock
+from repository.product_stock_repo import edit_product_quantity_in_stock
 from schemas.products_stock_schema import ProductStockOperation, ProductStockBuyOperation
-from controllers.product_stock_controller import edit_different_products_quantity_in_stock
+from controllers.product_stock_controller import edit_different_products_quantity_in_stock, get_all_products_from_stock_json
 
 router = APIRouter(
     prefix="/inventory",
@@ -13,17 +13,22 @@ router = APIRouter(
 
 @router.get("/")
 async def get_all_inventory_info_route():
-    products = get_all_products_from_stock()
-    return products
+    products = get_all_products_from_stock_json()
+    return JSONResponse(
+        content={
+            "data": products
+        },
+        status_code=status.HTTP_200_OK
+    )
 
 @router.put("/")
 async def edit_product_quantity_in_stock_route(product: ProductStockOperation):
     edit_product_quantity_in_stock(product.id, product.data, product.operation)
     return JSONResponse(
         content={
-            "message": "successfull operation"
+            "message": "successful operation"
         },
-        status_code=status.HTTP_200_OK
+        status_code=status.HTTP_202_ACCEPTED
     )
 
 @router.put("/buy-operation")
@@ -33,5 +38,5 @@ async def edit_different_products_quantity_in_stock_route(product: List[ProductS
         content={
             "message": "successful operation"
         },
-        status_code=status.HTTP_200_OK
+        status_code=status.HTTP_202_ACCEPTED
     )
